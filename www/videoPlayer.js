43aa8899ef57683d1da1a48935da0171470339aa
Nikola Peevski <nikola.peevski@mentormate.com>
2021-04-01 14:49:23 +0300

    Update input params Remove embed

--- www/videoPlayer.js
+++ www/videoPlayer.js
@@ -1,6 +1,6 @@
 var exec = require("cordova/exec");
 
-exports.play = function (videoId, success, error) {
+exports.play = function (accountId, policyKey, videoId, success, error) {
   exec(success, error, "BrightcovePlayer", "play", [
     accountId,
     policyKey,
