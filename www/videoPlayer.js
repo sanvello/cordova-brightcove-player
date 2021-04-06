b7200eee37dbc8b75f1d84c954dc228c2f0217b6
Nikola Peevski <nikola.peevski@mentormate.com>
2021-04-06 12:47:15 +0300

    Add exec for playByUrl

--- www/videoPlayer.js
+++ www/videoPlayer.js
@@ -8,10 +8,6 @@ exports.playById = function (accountId, policyKey, videoId, success, error) {
   ]);
 };
 
-// exports.init = function(policyKey, accountId, success, error) {
-//     exec(success, error, 'BrightcovePlayer', 'initAccount', [policyKey, accountId])
-// };
-
-// exports.switchAccountTo = function(policyKey, accountId, success, error) {
-//     exec(success, error, 'BrightcovePlayer', 'switchAccount', [policyKey, accountId])
-// }
+exports.playByUrl = function (videoUrl, success, error) {
+  exec(success, error, "BrightcovePlayer", "playByUrl", [videoUrl]);
+};
